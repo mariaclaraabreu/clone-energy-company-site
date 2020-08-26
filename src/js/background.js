bea661fee25cfcda4e5d36b3f1756d7525931e42
Maria Clara <mclara.engsoftware@gmail.com>
2020-08-26 14:03:05 -0300

    changing background colors

---
+++ src/js/background.js
@@ -0,0 +1,16 @@
+
+function change_background1(){
+  var obj=document.getElementById('body');
+  obj.className = 'background1';
+}
+
+function change_background2(){
+  var obj=document.getElementById('body');
+  obj.className = 'background2';
+}
+
+function change_background3(){
+  var obj=document.getElementById('body');
+  obj.className = 'background3';
+}
+
