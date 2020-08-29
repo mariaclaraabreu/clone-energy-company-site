9194ac12abf9a92e9447fce657d425b8691d520c
Maria Clara <mclara.engsoftware@gmail.com>
2020-08-29 09:47:53 -0300

    code refactoring

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,3 +1,8 @@
+const messages =[];
+var id = 0;
+const idLetters = 'item';
+
+
 var form = document.getElementById('form');
 
 
@@ -6,7 +11,7 @@ form.addEventListener('submit', function(e) {
     var name = document.getElementById('name');
     var message = document.getElementById('message');
 
-    const list = document.querySelector('.list-comments');
+   
 
     // var list = document.getElementsByClassName('list-comments');
     var item = document.createElement('li');
@@ -26,26 +31,72 @@ form.addEventListener('submit', function(e) {
     var but = document.createElement('button');
     var txtBut = document.createTextNode('Excluir');
     but.appendChild(txtBut);
-    // but.setAttribute('class', 'btn');
+    // var idItem = idLetters + cont++;
+    // console.log("aaaaaa" + idItem);
+    id = id + 1;
+    var te = idLetters + id;
+    console.log("aaaaaaa" + te);
+    but.setAttribute('id', te);
 
     // const butExc = document.querySelector('.btn');
 
+    but.setAttribute('class', 'item-list');
 
-    but.addEventListener('click', () => {
-      alert("deu certoooooo");
-    })
+    // but.addEventListener('click', () => {
+    //   alert("deu certoooooo");
+    // })
 
 
     item.appendChild(txtMessage);
     item.appendChild(but);
 
+    messages.push(item);
+    console.log(messages);
+    
 
+    // list.appendChild(item);
 
-    list.appendChild(item);
-
-    alert(name.value + "mensagem: " + message.value);
+    // alert(name.value + "mensagem: " + message.value);
     
 
     
     e.preventDefault();
+
+    
+    mostrarLista();
+    excluir();
 });
+
+
+function mostrarLista(){
+  const list = document.querySelector('.list-comments');
+  for(var i = 0; i < messages.length; i++){
+    // let item = document.createElement('li');
+    // item.appendChild(document.createTextNode(nomes[i]));
+    list.appendChild(messages[i]);
+}
+}
+
+
+function excluir(){
+  const itemList = document.querySelector('.item-list');
+
+  itemList.
+  console.log("iiiiiii" + itemList.value);
+
+
+  var x = document.getElementByClassName("item-list");
+  var meuId = x.id;
+
+  itemList.addEventListener('click', () => {
+    //pega o id do item
+
+
+    alert("oiiiiiiiiiiiii" + meuId);
+  });
+
+  mostrarLista();
+
+}
+
+
